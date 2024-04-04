c88cae58cb3e6f57056e5c0dd0bb9a3c0db83f38
Abdusattar2005 <abdaliuuluabdusattar2005@gmail.com>
2024-04-04 20:11:25 +0600

    fail

---
+++ Console Projects/Product.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace Console_Projects
+{
+    public class Product
+    {
+        static Product()
+        {
+            Console.WriteLine("STAIC COSNTRUCTOR");
+        }
+    
+        public Product()
+        {
+
+        }
+        public Product(string name, decimal price) 
+        {
+            Name = name;
+            Price = price;
+        }
+        public Product(string name, string articul, string description, decimal price, decimal markUp)
+        {
+            Name = name;
+            Articul = articul;
+            Description = description;
+            Price = price;
+            OptPrice = OptPrice + markUp;
+        }
+
+        public string Name;
+        public string Articul;
+        public string Description;
+
+        public decimal Price;
+        private decimal OptPrice = 25;
+    }
+
+    
+}
